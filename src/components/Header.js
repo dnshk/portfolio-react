51f5633d92e2ee97ec963baf575f05c76bbbd118
Alex Denyshchyk <oleksii.denyshchyk@gmail.com>
2022-01-12 17:20:49 -0800

    added script to header, added main.js with shared functions

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,57 @@
 import React, { useEffect, useState } from 'react';
+import {onscroll, select} from '../js/main.js'
 
 function Header() {
+    const [scrolling, setScrolling] = useState(false);
+    const [activeLink, setActive] = useState(false);
 
+    useEffect(() => {
+        const headerScrolled = () => {
+            if (window.scrollY > 100) {
+              setScrolling(true);
+            } else {
+              setScrolling(false);
+            }
+        }
+        window.addEventListener('load', headerScrolled);
+        onscroll(document, headerScrolled);
+
+        let navbarlinks = select('#navbar .scrollto', true);
+        const navbarlinksActive = () => {
+          let position = window.scrollY + 100
+          navbarlinks.forEach(navbarlink => {
+            if (!navbarlink.hash) return
+            let section = select(navbarlink.hash)
+            if (!section) return
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+              navbarlink.classList.add('active')
+            } else {
+              navbarlink.classList.remove('active')
+            }
+          })
+        }
+        window.addEventListener('load', navbarlinksActive);
+        onscroll(document, navbarlinksActive);
+    });
     return (
-        <div className="container d-flex align-items-center justify-content-between">
-            <h1 className="logo">
-                <a href="index.html">
-                    Oleksii Denyshchyk
-                </a>
-            </h1>
-            <nav id="navbar" className="navbar">
-                <ul>
-                    <li><a className="nav-link scrollto active" href="#hero">Home</a></li>
-                    <li><a className="nav-link scrollto" href="#about">About</a></li>
-                    <li><a className="nav-link scrollto " href="#work">Work</a></li>
-                    <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
-                </ul>
-                <i className="bi bi-list mobile-nav-toggle"></i>
-            </nav>
-        </div>
+        <header id="header" className={scrolling ?  'header-scrolled fixed-top' : 'fixed-top'}>
+            <div className="container d-flex align-items-center justify-content-between">
+                <h1 className="logo">
+                    <a href="index.html">
+                        Oleksii Denyshchyk
+                    </a>
+                </h1>
+                <nav id="navbar" className='navbar'>
+                    <ul>
+                        <li><a className="nav-link scrollto active" href="#hero">Home</a></li>
+                        <li><a className="nav-link scrollto" href="#about">About</a></li>
+                        <li><a className="nav-link scrollto " href="#work">Work</a></li>
+                        <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
+                    </ul>
+                    <i className="bi bi-list mobile-nav-toggle"></i>
+                </nav>
+            </div>
+        </header>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -12,13 +12,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
-
 reportWebVitals();
