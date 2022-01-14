2b146d06fae98e5440a0b9762dcfb1a4b0f97f68
Alex Denyshchyk <oleksii.denyshchyk@gmail.com>
2022-01-14 13:06:18 -0800

    added typed text to the hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import bgImg from '../img/hero-bg.jpg';
+import Typed from 'typed.js';
+import {select} from '../js/main.js'
 
 function Hero() {
+    useEffect(() => {
+        const typed = select('.typed');
+        if (typed) {
+            let typed_strings = typed.getAttribute('data-typed-items');
+            typed_strings = typed_strings.split(',');
+            new Typed('.typed', {
+                strings: typed_strings,
+                loop: true,
+                typeSpeed: 100,
+                backSpeed: 50,
+                backDelay: 2000,
+            });
+        }
+    });
     return (
         <div id="hero" className="hero route bg-image" style={{backgroundImage: "url(" + bgImg + ")"}}>
             <div className="overlay-intro"></div>
             <div className="hero-content display-table">
                 <div className="table-cell">
                     <div className="container">
-                        <h2 className="hero-title mb-3">Full Stack Web Developer</h2>
+                        <p class="hero-subtitle">
+                            <span class="typed" data-typed-items="Oleksii Denyshchyk"></span>
+                        </p>
+                        <h2 className="hero-title mb-3">
+                            Full Stack Web Developer
+                        </h2>
                         <p className="pt-3">
                             <a className="btn btn-primary btn js-scroll px-4" href="#about" role="button">
                                 Learn More
