ea6d8e226bfc517895397972ef95380c16ba14cc
Alex Denyshchyk <epicfail315@gmail.com>
2022-01-19 22:13:48 -0800

    added custom hook for modal toggle

---
+++ src/js/useToggles.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+​
+export const useToggles = (initialToggles) => {
+  const [toggles, setToggles] = useState(initialToggles);
+​
+  const handleToggles = (name, value) => {
+    setToggles({ ...toggles, [name]: value });
+  };
+​
+  return {
+    toggles,
+    handleToggles
+  };
+};
