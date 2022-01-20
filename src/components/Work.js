6e659255e7a5413bbe4e721017950f6537ebd9dd
Alex Denyshchyk <epicfail315@gmail.com>
2022-01-19 22:24:03 -0800

    added toggle functionality to the component, fixed typos

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import 'bootstrap';
+import { useToggles } from '../js/useToggles.js';
 
 function Work() {
     const [updateData, setUpdateData] = useState('');
@@ -24,6 +25,13 @@ function Work() {
     useEffect(() => {
         getData();
     }, [updateData]);
+
+    const { toggles, handleToggles } = useToggles({
+        isHarvadaModalOpen: false,
+        isEveryDropModalOpen: false,
+        isSportHubModalOpen: false,
+        isFanCheckModalOpen: false
+      });
 
     return (
         <section id="work" className="portfolio-mf sect-pt4 route">
@@ -90,104 +98,6 @@ function Work() {
                     )
                 }
                 </div>
-                {/* <div className="row">
-                    <div className="col-md-4">
-                        <div className="work-box">
-                            <a href="assets/img/work-1.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox">
-                                <div className="work-img">
-                                    <img src="assets/img/work-1.jpg" alt="" className="img-fluid" />
-                                </div>
-                            </a>
-                            <div className="work-content">
-                                <div className="row">
-                                    <div className="col-sm-8">
-                                        <h2 className="w-title">SportHub Website</h2>
-                                        <div className="w-more">
-                                            <span className="w-ctegory">Website</span> / <span className="w-date"> Jan - Apr, 2021</span>
-                                        </div>
-                                    </div>
-                                    <div className="col-sm-4">
-                                        <div className="w-like">
-                                            <a href="portfolio-details-1.html"> <span className="bi bi-plus-circle" /></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="work-box">
-                            <a href="assets/img/Devtools/dev-tools-1.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox">
-                                <div className="work-img">
-                                    <img src="assets/img/Devtools/dev-tools-1.jpg" alt="" className="img-fluid" />
-                                </div>
-                            </a>
-                            <div className="work-content">
-                                <div className="row">
-                                    <div className="col-sm-8">
-                                        <h2 className="w-title">DevTools Website</h2>
-                                        <div className="w-more">
-                                            <span className="w-ctegory">Website</span> / <span className="w-date">May, 2021</span>
-                                        </div>
-                                    </div>
-                                    <div className="col-sm-4">
-                                        <div className="w-like">
-                                            <a href="portfolio-details-2.html"> <span className="bi bi-plus-circle" /></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="work-box">
-                            <a href="assets/img/WP-Designs/wp-designs-1.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox">
-                                <div className="work-img">
-                                    <img src="assets/img/WP-Designs/wp-designs-1.jpg" alt="" className="img-fluid" />
-                                </div>
-                            </a>
-                            <div className="work-content">
-                                <div className="row">
-                                    <div className="col-sm-8">
-                                        <h2 className="w-title">WordPress Designs</h2>
-                                        <div className="w-more">
-                                            <span className="w-ctegory">Web Design</span> / <span className="w-date">May. 2021</span>
-                                        </div>
-                                    </div>
-                                    <div className="col-sm-4">
-                                        <div className="w-like">
-                                            <a href="portfolio-details-3.html"> <span className="bi bi-plus-circle" /></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="work-box">
-                            <a href="assets/img/work-2.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox">
-                                <div className="work-img">
-                                    <img src="assets/img/work-2.jpg" alt="" className="img-fluid" />
-                                </div>
-                            </a>
-                            <div className="work-content">
-                                <div className="row">
-                                    <div className="col-sm-8">
-                                        <h2 className="w-title">Fan Check App Website</h2>
-                                        <div className="w-more">
-                                            <span className="w-ctegory">Web Design</span> / <span className="w-date"> Aug, 2020</span>
-                                        </div>
-                                    </div>
-                                    <div className="col-sm-4">
-                                        <div className="w-like">
-                                            <a href="portfolio-details-4.html"> <span className="bi bi-plus-circle" /></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div> */}
             </div>
         </section>
     );
--- src/js/useToggles.js
+++ src/js/useToggles.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
-​
+
 export const useToggles = (initialToggles) => {
   const [toggles, setToggles] = useState(initialToggles);
-​
+
   const handleToggles = (name, value) => {
     setToggles({ ...toggles, [name]: value });
   };
-​
+
   return {
     toggles,
     handleToggles
