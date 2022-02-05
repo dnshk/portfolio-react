b9eaa1bf9c6f419e9539676d3a70f3953ec3f2df
Alex Denyshchyk <epicfail315@gmail.com>
2022-02-04 21:47:19 -0800

    indentation fix

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 reportWebVitals();
