942d08e6a104c1fe44e20b4e8260e2a0b1518940
Alex Denyshchyk <epicfail315@gmail.com>
2022-02-04 21:49:48 -0800

    indentation fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,45 +1,45 @@
 import React, { useEffect } from 'react';
-import { select, onscroll } from '../js/main.js'
+import { select, onscroll } from '../js/main.js';
 
 function Footer() {
     useEffect(() => {
         let preloader = select('#preloader');
         if (preloader) {
             window.addEventListener('load', () => {
-                preloader.remove()
+                preloader.remove();
             });
         }
-        let backtotop = select('.back-to-top')
+        let backtotop = select('.back-to-top');
         if (backtotop) {
-          const toggleBacktotop = () => {
-            if (window.scrollY > 100) {
-              backtotop.classList.add('active')
-            } else {
-              backtotop.classList.remove('active')
+            const toggleBacktotop = () => {
+                if (window.scrollY > 100) {
+                    backtotop.classList.add('active');
+                } else {
+                    backtotop.classList.remove('active');
+                }
             }
-          }
-          window.addEventListener('load', toggleBacktotop)
-          onscroll(document, toggleBacktotop)
+            window.addEventListener('load', toggleBacktotop);
+            onscroll(document, toggleBacktotop);
         }
     });
     return (
         <>
-        <footer>
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-12">
-                        <div className="copyright-box">
-                            <p className="copyright">
-                                2021-2022 <strong>Alex Denyshchyk</strong>
-                            </p>
+            <footer>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <div className="copyright-box">
+                                <p className="copyright">
+                                    2021-2022 <strong>Alex Denyshchyk</strong>
+                                </p>
+                            </div>
                         </div>
+                        <a className="repo-link" href="https://github.com/dnshk/portfolio-react">Repository for this website</a>
                     </div>
-                    <a className="repo-link" href="https://github.com/dnshk/portfolio-react">Repository for this website</a>
                 </div>
-            </div>
-        </footer>
-        <div id="preloader"></div>
-        <a href="#hero" className="back-to-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short"></i></a>
+            </footer>
+            <div id="preloader"></div>
+            <a href="#hero" className="back-to-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short"></i></a>
         </>
     );
 }
