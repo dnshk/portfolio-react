052619ae9acceeb9b1523f7c820f8bb4191c9563
Alex Denyshchyk <epicfail315@gmail.com>
2022-02-07 22:30:38 -0800

    added early returns for select and on functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,18 @@
 export const select = (el, all = false) => {
     el = el.trim();
-    if (all) {
-        return [...document.querySelectorAll(el)];
-    } else {
-        return document.querySelector(el);
-    }
+    if (all) return [...document.querySelectorAll(el)];
+
+    else return document.querySelector(el);
 }
 
 export const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all);
-    if (selectEl) {
-        if (all) {
-            selectEl.forEach(e => e.addEventListener(type, listener));
-        } else {
-            selectEl.addEventListener(type, listener);
-        }
-    }
+    const selectEl = select(el, all);
+    if (!selectEl) return
+
+    if (all) return selectEl.forEach(e => e.addEventListener(type, listener))
+
+    selectEl.addEventListener(type, listener);
+
 }
 
 export const onscroll = (el, listener) => {
